8703b5b2ae9fc93f3ae23a115ea8d91d7d920630
Tushar2612 <118872829+Abhishek7738@users.noreply.github.com>
2026-08-25 18:06:29 +0530

    Improve responsive top bar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,17 +1,16 @@
 function TopBar() {
   return (
-    <div className="w-full h-[35px] bg-[#0666d6] text-white overflow-hidden">
-      <div className="flex w-max animate-[marquee_30s_linear_infinite]">
-        <span className="whitespace-nowrap px-[60px] py-[8px]">
-          Welcome to Megsale! Wrap new offers/gift every single day on Weekends.
-          New Coupon Code: FAST50
+    <div className="w-full h-9 bg-blue-600 text-white overflow-hidden">
+      <div className="flex w-max items-center animate-[marquee_30s_linear_infinite]">
+        <span className="whitespace-nowrap px-8 sm:px-12 py-2 text-xs sm:text-sm  font-medium leading-none">
+          🎉 Welcome to Megsale!
         </span>
-        <span className="whitespace-nowrap px-[60px] py-[8px]">
-          Something you love is now on sale — Buy Now!
+        <span className="whitespace-nowrap px-8 sm:px-12 py-2 text-xs sm:text-sm  font-medium leading-none">
+          🔥 New Coupon Code: FAST50
         </span>
 
-        <span className="whitespace-nowrap px-[60px] py-[8px]">
-          Your must-have item is calling — Buy Now!
+        <span className="whitespace-nowrap px-8 sm:px-12 py-2 text-xs sm:text-sm  font-medium leading-none">
+          🛍️ Something you love is now on sale — Buy Now!
         </span>
       </div>
     </div>
